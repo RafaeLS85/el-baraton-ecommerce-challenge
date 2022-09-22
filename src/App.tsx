c49efe4c44e2c9506b17a0aa8634e7d184e3a17e
Rafael Olivay <rafaelsebastian@live.com.ar>
2022-09-22 19:27:04 -0300

    menu recursivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { categories } from "./data/categories.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+type Category = {
+  id: number;
+  name: string;
+  sublevels?: Category[]
+}
+type MenuProps = {
+  categories: Category[]
 }
 
-export default App
+function Menu({categories} : MenuProps ) {
+  return (
+    <div>
+      <ol>
+        {categories.map((category) => (
+          <li key={category.id}> 
+           <span>{category.name}</span>
+           { category.sublevels && <Menu categories={category.sublevels} /> }   
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Menu categories={categories}/>
+    </div>
+  );
+}
+
+export default App;
